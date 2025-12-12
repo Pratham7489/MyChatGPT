b1d19a9591057cf9d952435a3209dfc1d29aaa44
Pratham Patel <ppratham134patel@gmail.com>
2025-12-13 02:05:53 +0530

    update the vercel frontend link

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",        
-      "https://mychatgpt-seven-theta.vercel.app/"  
+      "https://mychatgpt-seven-theta.vercel.app"  
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
